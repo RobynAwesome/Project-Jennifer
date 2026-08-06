6e108ff643b32c8645827d359ce3cf1662fae67f
Kholofelo Robyn Rababalela <rkholofelo@gmail.com>
2026-08-06 09:49:12 +0200

    feat: export NCMP and PERN foundations

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,3 +3,5 @@ export * from "./event-bus.js";
 export * from "./utils.js";
 export * from "./poc-foc.js";
 export * from "./companions.js";
+export * from "./ncmp.js";
+export * from "./pern-foundation.js";
