b9f222ca9ad3265c6d09f17bef53051450b14009
Kholofelo Robyn Rababalela <rkholofelo@gmail.com>
2026-08-08 22:47:55 +0200

    feat(memory): export memory receipt engine

--- packages/memory/src/index.ts
+++ packages/memory/src/index.ts
@@ -12,3 +12,27 @@ export type {
   PersistedMemoryEntry,
   ObjectiveWeightVector,
 } from "./gsmb.js";
+
+export {
+  ARPM_RESEARCH_PROFILE,
+  MEMORY_RECEIPT_INVARIANTS,
+  MemoryReceiptEngine,
+  RELATIONAL_FAILURE_VECTORS,
+  buildRiskVectorMatrix,
+  validateMemoryReceiptInput,
+} from "./memory-receipt-engine.js";
+
+export type {
+  MemoryReceipt,
+  MemoryReceiptInput,
+  MemoryReceiptRiskAnalysis,
+  ReceiptAdmission,
+  ReceiptConceptState,
+  ReceiptMemoryLane,
+  RelationalFailureVector,
+  RetrievalValidationTrace,
+  RiskVectorMatrix,
+  RiskVectorObservation,
+  RiskVectorObservations,
+  TemporalGovernance,
+} from "./memory-receipt-engine.js";
