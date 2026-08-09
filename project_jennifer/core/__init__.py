372a0110f1b3398ab01c55ddd9671e57e1fb3b64
Kholofelo Robyn Rababalela <rkholofelo@gmail.com>
2026-08-09 22:22:51 +0200

    feat(core): export renter router with governed Free Mode

--- project_jennifer/core/__init__.py
+++ project_jennifer/core/__init__.py
@@ -1,5 +1,12 @@
 """Core Free Mode engine interfaces."""
 
 from .free_mode_engine import FreeModeEngine, FreeModeRequest, FreeModeResult
+from .renter_router import RenterRegistry, StatelessRenterRouter
 
-__all__ = ["FreeModeEngine", "FreeModeRequest", "FreeModeResult"]
+__all__ = [
+    "FreeModeEngine",
+    "FreeModeRequest",
+    "FreeModeResult",
+    "RenterRegistry",
+    "StatelessRenterRouter",
+]
