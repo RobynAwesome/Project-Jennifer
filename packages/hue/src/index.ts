390ac5e4dbd1028a2cc1721fa188e4f2a8f45c6b
Kholofelo Robyn Rababalela <rkholofelo@gmail.com>
2026-08-15 21:42:54 +0200

    feat(hue): export symbolic profile store

--- packages/hue/src/index.ts
+++ packages/hue/src/index.ts
@@ -4,3 +4,9 @@ export {
   ContextWeighter,
   BehavioralAdapter,
 } from "./hue-engine.js";
+
+export { SymbolicProfileStore } from "./symbolic-profile.js";
+export type {
+  HumanSymbolicProfile,
+  ZodiacProfileSignal,
+} from "./symbolic-profile.js";
