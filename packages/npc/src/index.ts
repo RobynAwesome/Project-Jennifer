f2e325bf3026b1830ed8d85fd7f69fbcfc2ed91b
Kholofelo Robyn Rababalela <rkholofelo@gmail.com>
2026-08-24 02:54:20 +0200

    feat(npc): export divergence runtime

--- packages/npc/src/index.ts
+++ packages/npc/src/index.ts
@@ -5,3 +5,27 @@ export {
   NPCRegistry,
 } from "./npc-runtime.js";
 export type { NPCGoal, LocalAwarenessSnapshot } from "./npc-runtime.js";
+
+export {
+  EpistemicDivergenceEngine,
+  EpistemicDivergenceError,
+} from "./epistemic-divergence.js";
+export type {
+  ActorObservation,
+  ConsequenceRule,
+  ConsequenceVisibility,
+  DivergenceActorContext,
+  DivergenceAwarenessSnapshot,
+  DivergenceCapability,
+  DivergenceConsequenceIntent,
+  DivergenceEvent,
+  DivergenceEventFact,
+  DivergenceGoalSnapshot,
+  EpistemicDisposition,
+  EpistemicDivergenceInput,
+  EpistemicDivergenceReceipt,
+  EpistemicDivergenceThresholds,
+  InterpretationCandidate,
+  InterpretationLabel,
+  ObservationMeaning,
+} from "./epistemic-divergence.js";
